3b0c4988cbe1fa78130242ff8a42052a72b0356d
shahmohak <89686625+shahmohak@users.noreply.github.com>
2023-09-15 02:08:38 +0530

    Basic_Directions.cpp
    Changing the skeleton of the code, making it compatible with the basic 4 directions that is North, South, West, and East. As well as making it support the movement in forward and backward direction in all 4 directions.

--- Forward_Backward.cpp
+++ Forward_Backward.cpp
@@ -4,40 +4,80 @@ using namespace std;
 class LunarCraft {
 private:
     int x, y, z; // Current position (x, y, z)
-    char direction;// Current direction (N, S, E, W, U, D)
+    char direction; // Current direction (N, S, E, W, U, D)
+    char sub;
 
 public:
     LunarCraft() {
         x = 0;
         y = 0;
         z = 0;
-        direction = 'N';
+        direction = 'N'; // Initial direction is North
     }
 
     void move(char command) {
         if (command == 'f') {
             moveForward();
-        } else{
+        } else if (command == 'b') {
             moveBackward();
+        } else if (command == 'l') {
+            turnLeft();
+        } else{
+            turnRight();
         } 
     }
 
     void moveForward() {
         if (direction == 'N') {
             y++;
-        } else {
+        } else if (direction == 'S') {
             y--;
+        } else if (direction == 'E') {
+            x++;
+        } else {
+            x--;
         }
     }
 
     void moveBackward() {
         if (direction == 'N') {
             y--;
-        } else {
+        } else if (direction == 'S') {
             y++;
-        } 
+        } else if (direction == 'E') {
+            x--;
+        } else {
+            x++;
+        }
     }
-   void printPosition() {
+
+    void turnLeft() {
+        if (direction == 'N') {
+            direction = 'W';
+        } else if (direction == 'S') {
+            direction = 'E';
+        } else if (direction == 'E') {
+            direction = 'N';
+        } else{
+            direction = 'S';
+        }
+    }
+
+    void turnRight() {
+        if (direction == 'N') {
+            direction = 'E';
+        } else if (direction == 'S') {
+            direction = 'W';
+        } else if (direction == 'E') {
+            direction = 'S';
+        } else{
+            direction = 'N';
+        }
+    }
+
+
+
+    void printPosition() {
         cout << "Final Position: (" << x << ", " << y << ", " << z << ")" << endl;
         cout << "Final Direction: " << direction << endl;
     }
@@ -45,7 +85,7 @@ public:
 
 int main() {
     LunarCraft chandrayaan3;
-    char commands[] = {'f','b','f'};
+    char commands[] = {'f', 'r', 'l', 'f', 'l', 'b'};
 
     for (char command : commands) {
         chandrayaan3.move(command);
